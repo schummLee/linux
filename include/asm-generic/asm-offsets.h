2930719274a65212bfa5c9d32c1a3c2349e21e04
lee23 <71886857+schummLee@users.noreply.github.com>
2023-10-26 10:26:46 +0200

    Update asm-offsets.h

--- include/asm-generic/asm-offsets.h
+++ include/asm-generic/asm-offsets.h
@@ -1,1 +1,24 @@
+#ifndef ASM_OFFSET_H
+#define ASM_OFFSET_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <generated/asm-offsets.h>
+#include <stdint.h>
+
+#define GET_OFFSET(var, offset_ptr) \
+    __asm__ ("lea (%1), %0" : "=r"(offset_ptr) : "r"(&(var)))
+
+static inline uintptr_t get_offset(void* var) {
+    uintptr_t address;
+    __asm__ ("lea (%1), %0" : "=r"(address) : "r"(var));
+    return address;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
